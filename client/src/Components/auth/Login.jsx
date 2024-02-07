9ed2e437dc5a3d0852f363fdc1848513680ef4b9
pavithra-k20cs29 <pavithrakaruppusamy2003@gmail.com>
2024-02-07 19:27:29 +0530

    new commit

--- client/src/Components/auth/Login.jsx
+++ client/src/Components/auth/Login.jsx
@@ -31,7 +31,7 @@ export const Login = () => {
 
   const login = () => {
     axios
-      .post("https://mern-quiz-server-sudhir.onrender.com/login", user)
+      .post("https://backend-si6u.onrender.com/login", user)
       .then((res) => {
         if (res.data.user.email === "pavithrakaruppusamy20@gmail.com") {
           dispatch(loginAdminId(res.data.user._id));
